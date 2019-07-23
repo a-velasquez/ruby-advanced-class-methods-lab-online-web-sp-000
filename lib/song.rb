34472ff4b5b9c7c19b4f0040bc20c1eb96939221
Alex Velasquez <jtinnesco@gmail.com>
2019-07-23 03:30:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,7 +44,8 @@ class Song
   def self.new_from_filename(filename)
     split_filename = filename.chomp(".mp3").split(" - ")
     song = Song.new 
-    song.name = 
+    song.name = split_filename[1]
+    
   end
   
   
